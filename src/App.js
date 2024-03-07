501cd59462a0751a010ba96b065188225cb297a8
Bara87 <bara-2010@hotmail.fr>
2024-03-07 18:57:52 +0100

    App routes insertion

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import './index.css'
+import HomePage from "./pages/HomePage";
+import ServicesPage from "./pages/ServicesPage";
+import PortfolioPage from "./pages/PortfolioPage";
+import BlogPage from "./pages/BlogPage";
+import ContactPage from "./pages/ContactPage";
+import LegalPage from "./pages/LegalPage";
+import AproposPage from "./pages/AproposPage";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import './index.css';
+
 
 function App() {
-    return (
-        <div>
-            <Routes>
-                <Route></Route>
-                <Route></Route>
-                <Route></Route>
-                <Route></Route>
-                <Route></Route>
-                <Route></Route>
-                <Route></Route>
-            </Routes>
-        </div>
-    )
+  return (
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/apropos" element={<AproposPage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/services" element={<ServicesPage />} />
+        <Route path="/portfolio" element={<PortfolioPage />} />
+        <Route path="/blog" element={<BlogPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/legal" element={<LegalPage />} />
+      </Routes>
+      
+      <Footer />
+    </div>  )
 };
 export default App;
+
